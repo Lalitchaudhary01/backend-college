b4c6086dad6b0c2e7fdb0cb6749d0c4295514fe9
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2025-01-18 09:42:08 +0530

    add controller

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,3 @@
-const dotenv = require("dotenv");
-
-dotenv.config(); // Load environment variables from .env file
-
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  serverConfig: require("./server-config"),
 };
